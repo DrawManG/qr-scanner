59af644674af1aa1660b8c0f73fca29e6bc36535
anybody <we-are-legion2013@yandex.ru>
2023-07-12 17:43:50 +0300

    all completed
    need fix problem in server

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -27,7 +27,7 @@
   width: 100%;
   height: 100%;
   object-fit: fill;
-  display: none; /* Скрыть видео на других платформах */
+  display: none;
 }
 .hide-button {
   display: none;
@@ -41,44 +41,100 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from 
 <script lang="ts">
 import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
 import { alertController, isPlatform } from '@ionic/vue';
+import axios from 'axios';
 
 export default {
   data() {
     return {
-      isScanning: false, // Флаг состояния сканирования
+      isScanning: false, 
     };
   },
   methods: {
     async startScan() {
-      this.isScanning = true; // Установка флага сканирования в true
+      this.isScanning = true; 
 
-      // Check camera permission
+      
       await BarcodeScanner.checkPermission({ force: true });
 
-      // make background of WebView transparent
+      
       BarcodeScanner.hideBackground();
       document.querySelector('body')?.classList.add('scanner-active');
 
-      // Check platform
+
       if (isPlatform('android')) {
         const videoElement = document.getElementById('videoElement');
         videoElement.style.display = 'block';
 
         const result = await BarcodeScanner.startScan({ targetedFormats: 'QR_CODE', previewElement: videoElement });
 
-        // Обработка результата сканирования
-        if (result.hasContent) {
-          const alert = await alertController.create({
-            header: 'QR',
-            subHeader: 'Сканированные данные',
-            message: result.content,
-            buttons: ['OK'],
-          });
 
-          await alert.present();
+        if (result.hasContent) {
+          try {
+            let message = result.content;
+
+            if (!message.includes('georeport.ru')) {
+              throw new Error('QR-CODE не является кодом GeoReport');
+            }
+
+
+            const response = await axios.get('https://georeport.ru', {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 13; RMX3472 Build/TP1A.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/114.0.5735.196 Mobile Safari/537.36',
+    'Accept': 'application/json',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Host': 'georeport.ru',
+    'Referer': 'https://localhost/',
+  },
+  timeout: 5000,
+});
+
+
+
+            const html = response.data;
+            const parser = new DOMParser();
+            const htmlDoc = parser.parseFromString(html, 'text/html');
+
+            const table = htmlDoc.querySelector('.table__tbody');
+            const rows = table ? table.querySelectorAll('.table__tr') : [];
+
+            let parsedData = '';
+            if (rows.length > 0) {
+              rows.forEach(row => {
+                const cells = row.querySelectorAll('.table__td');
+                const label = cells[0].textContent.trim();
+                const value = cells[1].textContent.trim();
+                parsedData += `${label}: ${value}\n`;
+              });
+            } else {
+              parsedData = 'Ошибка чтения кода';
+            }
+
+            message = parsedData;
+
+            const alert = await alertController.create({
+              header: 'QR',
+              subHeader: 'Сканированные данные',
+              message: message,
+              buttons: ['OK'],
+            });
+
+            await alert.present();
+          } catch (error) {
+            if (error instanceof Error) {
+              const alert = await alertController.create({
+                header: 'Ошибка',
+                message: error.message,
+                buttons: ['OK'],
+              });
+
+              await alert.present();
+            } else {
+              console.error(error);
+            }
+          }
         }
       } else {
-        // For other platforms, continue with the existing logic
+        
         const result = await BarcodeScanner.startScan();
 
         document.querySelector('body')?.classList.remove('scanner-active');
@@ -95,8 +151,9 @@ export default {
         }
       }
 
-      this.isScanning = false; // Установка флага сканирования в false
+      this.isScanning = false; 
     }
   }
 }
+
 </script>
